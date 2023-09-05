468c55d50794180f044755c1bbd94c929f3df94f
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-09-05 23:18:15 +0300

    Update file

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,8 @@
+module Api
+    class GreetingsController < ApplicationController
+      def random
+        random = Greeting.order('RANDOM()').first
+        render json: { greeting: random }
+      end
+    end
+  end
--- app/controllers/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class GreetingsController < ApplicationController
-    def random
-        random_greeting = Greeting.order("RANDOM()").first
-        render json: random_greeting
-    end
-end
